19fd0048d44c557966e46073e9762ec99376600a
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-27 16:43:48 -0500

    create tk_main.py to create tkinter

---
+++ tk_main.py
@@ -0,0 +1,56 @@
+# import the packages
+from tkinter import *
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import numpy as np
+import matplotlib.pyplot as plt
+
+# reading the file and assigning X, y
+def readFile(file_path):
+    dataset = pd.read_csv(file_path)
+    X = dataset.iloc[:,0].values # Land Area (sqft)
+    y = dataset.iloc[:,1].values # Purchase Price ($M)
+    return X, y, dataset
+
+file_path = r'data/landprice.csv'
+X, y, df = readFile(file_path)
+
+# splitting the data
+def split_data(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = split_data(X, y)
+
+# training the model
+def model_train(X_train, y_train, X_test, y_test):
+    
+    X_train1 = np.reshape(X_train, (-1, 1))
+    y_train1 = np.reshape(y_train, (-1, 1))
+
+    X_test1 = np.reshape(X_test, (-1, 1))
+    y_test1 = np.reshape(y_test, (-1, 1))
+    
+    lin_regressor = LinearRegression()
+    lin_regressor.fit(X_train1, y_train1)
+
+    y_pred = lin_regressor.predict(X_test1)
+
+    return y_pred
+
+y_pred = model_train(X_train, y_train, X_test, y_test)
+
+# initialize tkinter window
+window = Tk()
+window.geometry('600x700')
+
+
+
+
+
+
+mainloop()
+
+
